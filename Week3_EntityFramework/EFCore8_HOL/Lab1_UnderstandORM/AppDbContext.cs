7187f3c24e2e111768733645c842d606c4e896f0
Dhruvkus-14 <dhruvkushwaha484@gmail.com>
2025-07-06 10:47:26 +0530

    Added  EF Core Labs 1 with code, screenshots, and README

---
+++ Week3_EntityFramework/EFCore8_HOL/Lab1_UnderstandORM/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Lab1_UnderstandORM.Models;
+
+namespace Lab1_UnderstandORM
+{
+    public class RetailDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // LocalDB connection string (you can update this if needed)
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=RetailInventoryDb;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Optional: Define relationships, keys, constraints, etc.
+            modelBuilder.Entity<Category>()
+                        .HasMany(c => c.Products)
+                        .WithOne(p => p.Category)
+                        .HasForeignKey(p => p.CategoryId);
+        }
+    }
+}
